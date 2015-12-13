3ee6e8d84a352886e2b2135f7f1a3c609f68830f
anonymoussc <50c5ac69@opayq.com>
2015-12-13 11:13:28 +0700

    Add angular directory

---
+++ angular/app.foundation.module.js
@@ -0,0 +1,38 @@
+/**
+ * Created by anonymous on 13/12/15 11:09.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('appFoundation', [
+            /* Angularjs */
+            'ngMaterial',
+            'ngMessages',
+            'ngResource',
+            'ngMessages',
+
+            /* 3rd-party */
+            'ui.router',
+            'satellizer',
+            'restangular',
+            'ngStorage',
+            'angular-loading-bar',
+            'ngMdIcons',
+            'toastr',
+            'vAccordion',
+
+            /* Intra-services */
+            'inServices.exception',
+            'inServices.logger',
+            'inServices.routes'
+        ]);
+
+    angular.module('widgets', []);
+
+    angular.module('inServices.exception', []);
+    angular.module('inServices.logger', []);
+    angular.module('inServices.routes', []);
+
+})();
