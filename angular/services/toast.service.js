b22d4b93f70f05caa03698e7b9798ae23f6e05fa
anonymoussc <50c5ac69@opayq.com>
2015-12-30 18:19:49 +0700

    Modified toast.service Deleted .gitkeep

---
+++ angular/services/toast.service.js
@@ -0,0 +1,60 @@
+/**
+ * Created by anonymous on 30/12/15 18:11.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('appFoundation')
+        .factory('toastService', toastService);
+
+    toastService.$inject = ['$mdToast'];
+
+    /* @ngInject */
+    function toastService($mdToast) {
+        var delay    = 6000,
+            position = 'top right',
+            action   = 'OK';
+
+        var service = {
+            show : show,
+            error: error
+        };
+
+        return service;
+
+        ////////////////
+
+        function show(content) {
+            if (!content) {
+                return false;
+            }
+
+            return $mdToast.show(
+                $mdToast.simple()
+                    .content(content)
+                    .position(position)
+                    .action(action)
+                    .hideDelay(delay)
+            );
+        }
+
+        function error(content) {
+            if (!content) {
+                return false;
+            }
+
+            return $mdToast.show(
+                $mdToast.simple()
+                    .content(content)
+                    .position(position)
+                    .theme('warn')
+                    .action(action)
+                    .hideDelay(delay)
+            );
+        }
+    }
+
+})();
+
