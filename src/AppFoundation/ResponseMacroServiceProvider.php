f92a28f9d6b1b0458e64b4507905d75efe5fad01
anonymoussc <50c5ac69@opayq.com>
2016-01-04 00:26:02 +0700

    New ResponseMacroServiceProvider

---
+++ src/AppFoundation/ResponseMacroServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by anonymous on 04/01/16 0:14.
+ */
+
+namespace Consigliere\AppFoundation;
+
+use Response;
+use Illuminate\Support\ServiceProvider;
+
+class ResponseMacroServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Response::macro('success', function ($data) {
+            return Response::json([
+                'errors' => false,
+                'data'   => $data,
+            ]);
+        });
+
+        Response::macro('error', function ($message, $status = 400) {
+            return \Response::json([
+                'message'     => $status . ' error',
+                'errors'      => [
+                    'message' => [$message],
+                ],
+                'status_code' => $status,
+            ], $status);
+        });
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
