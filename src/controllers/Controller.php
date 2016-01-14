55fc0e2f4a2f381acb0bd8db8d42b7cd5480e2ae
anonymoussc <50c5ac69@opayq.com>
2016-01-14 09:02:02 +0700

    New Controller

---
+++ src/controllers/Controller.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Controller.php
+ * Created by anonymous on 14/01/16 8:58.
+ */
+
+namespace Consigliere\AppFoundation\Controller;
+
+use Dingo\Api\Http\Request;
+use Dingo\Api\Exception\ValidationHttpException;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+/**
+ * Class Controller
+ * @package Consigliere\AppFoundation\Controller
+ */
+abstract class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @param Request $request
+     * @param array   $rules
+     * @param array   $messages
+     * @param array   $customAttributes
+     */
+    public function validate(Request $request, array $rules, array $messages = [], array $customAttributes = [])
+    {
+        $validator = $this->getValidationFactory()->make($request->all(), $rules, $messages, $customAttributes);
+
+        if ($validator->fails()) {
+            throw new ValidationHttpException($validator->errors());
+        }
+    }
+}
