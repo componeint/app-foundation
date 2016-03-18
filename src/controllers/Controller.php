991c21797670d312d5c48c5786817d813204ddf3
anonymoussc <50c5ac69@opayq.com>
2016-03-18 20:44:20 +0700

    Modified Controller

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -11,7 +11,7 @@ use Dingo\Api\Exception\ValidationHttpException;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 
 /**
  * Class Controller
